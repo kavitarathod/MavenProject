94aa82671af13729cfbb3ae8d967b6344135c2bf
Kavitha <kavitarathod27@gmail.com>
2019-02-16 16:22:29 +0530

    final commit.

--- src/test/java/com/pkg/Hello.java
+++ src/test/java/com/pkg/Hello.java
@@ -1,14 +1,25 @@
 package com.pkg;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Hello {
-
+	WebDriver driver;
 	
 
 		@Test
 		void printMsg() {
 			
-		System.out.println("Hello Kavitha");
+		System.out.println("Hello Kavitha");	
+		
+		String new1=System.getProperty("user.dir")+"\\Drivers";
+		System.out.println(new1);
+		System.setProperty("webdriver.chrome.driver",new1+"\\chromedriver.exe");
+		driver=new ChromeDriver();
+		//navigating to url
+		driver.get("https://www.google.com/");
+		//closing the browser
+		driver.close();
 	}
 
 }
